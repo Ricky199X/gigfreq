ef45274e510a5862b655660f6e8d274245c892bf
Ricky199X <rickyrojas19@gmail.com>
2019-10-12 19:43:29 -0400

    stubs out show controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,38 @@
 class ShowsController < ApplicationController
+    def index
+        @shows = Show.all
+    end
+
+    def new
+        @show = Show.new
+    end
+
+    def create
+        @show = Show.create(show_params)
+    end
+
+    def show
+        @show = Show.find(params[:id])
+    end
+
+    def edit
+        @show = Show.find_by(id: params[:id])
+    end
+
+    def update
+        @show = Show.find_by(id: params[:id])
+        @show.update(show_params)
+
+        if @show.save
+            redirect_to edit_show_path(@show)
+        else
+            flash[:alert] = "Show Profile not saved"
+        end
+    end
+
+    private
+
+    def show_params
+        params.require(:show).permit(:name, :venue, :city, :state, :date)
+    end
 end
