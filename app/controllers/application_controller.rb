603febe86d570b744bba8ba54beeccb43fe952fc
Ricky199X <rickyrojas19@gmail.com>
2019-10-12 22:35:03 -0400

    adds helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < ActionController::Base
     # this is so we can pass other user params aside from email and password
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def home
-        
+    helper_method :current_user
+
+    def logged_in?
+        redirect_to '/' if !current_user
+    end
+
+    def band?
+        redirect_to bands_path if !current_user.class == User
+    end
+
+    private
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def current_band
+        @current_band ||= Band.find(session[:band_id]) if session[:band_id]
     end
 
     protected 
