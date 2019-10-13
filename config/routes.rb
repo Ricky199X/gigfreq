e99d1252b85720d735e9e19900db4b86fd88468e
Ricky199X <rickyrojas19@gmail.com>
2019-10-13 10:08:08 -0400

    adds validation errors to user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :users 
+  resources :users
   resources :bands do
     resources :shows
   end
-
   resources :shows
 
-  # get '/bands/sign_up' to: 'bands#new'
- 
   root to: 'application#home'
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
