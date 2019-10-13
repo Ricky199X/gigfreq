610a4490cfe967dc79a87c5828a116ac37b831c6
Ricky199X <rickyrojas19@gmail.com>
2019-10-13 12:08:07 -0400

    remigrates to include polymorphic relationship

--- db/migrate/20191012230617_create_bands.rb
+++ db/migrate/20191012230617_create_bands.rb
@@ -1,9 +1,6 @@
 class CreateBands < ActiveRecord::Migration[6.0]
   def change
     create_table :bands do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
       t.string :website
       t.string :genre
       t.text :description
--- db/migrate/20191013130658_create_users.rb
+++ db/migrate/20191013130658_create_users.rb
@@ -1,11 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
       t.string :city
       t.string :state
-      t.string :password_digest
       t.string :favorite_band
     end
   end
