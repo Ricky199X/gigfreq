e71f62313d2e0b11bbd13e2fef90449fef9d8589
Ricky199X <rickyrojas19@gmail.com>
2019-10-13 09:11:39 -0400

    deletes devise migrations + adds user migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  
+  resources :users 
   resources :bands do
     resources :shows
   end
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   # get '/bands/sign_up' to: 'bands#new'
  
-
   root to: 'application#home'
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20191011230653_add_user_params_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddUserParamsToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :city, :string
-    add_column :users, :state, :string
-    add_column :users, :favorite_band, :string
-  end
-end
