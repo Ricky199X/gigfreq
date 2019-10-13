cab2cdd25617684fc2056b8919332f0a32b9309d
Ricky199X <rickyrojas19@gmail.com>
2019-10-13 09:47:07 -0400

    adds validations + associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: true
+  validates :password, uniqueness: true
 
-  # has_many :user_shows
-  # has_many :user_bands
-  # has_many :shows, through: :user_shows
-  # has_many :bands, through: :user_bands
+  has_many :user_shows
+  has_many :user_bands
+  has_many :shows, through: :user_shows
+  has_many :bands, through: :user_bands
 end
