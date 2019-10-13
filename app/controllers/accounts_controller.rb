347d4294eddf5a55d59bd2e0c0186a1c5eee9c36
Ricky199X <rickyrojas19@gmail.com>
2019-10-13 14:43:16 -0400

    adds create method to accountscontroller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,16 +5,19 @@ class AccountsController < ApplicationController
     end
 
     def create
-        # if checkbox is checked, account_type = band
         @account = Account.create(account_params)
-        
+        if params[:is_band?] == true
+            redirect_to new_band_path
+        else
+            redirect_to new_user_path
+        end
     end
 
 
     private
 
     def account_params
-        params.require(:account).permit(:username, :email, :password)
+        params.require(:account).permit(:username, :email, :password, :is_band?)
     end
     
 end
