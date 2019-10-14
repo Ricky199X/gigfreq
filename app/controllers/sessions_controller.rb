5075d2868dc383b410890b6f9cc0a912250b3022
Ricky199X <rickyrojas19@gmail.com>
2019-10-14 07:03:22 -0400

    logic to redirect login based on accountable type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @account = Account.find_by(email: params[:account][:email])
+        @account = Account.find_by(email: params[:email])
         binding.pry
         if !@account.nil? && @account.authenticate(params[:account][:password])
             log_in(@account)
-            redirect_to account_path(@account)
+            # if accountable_type == "Band", redirect to band path
+            if @account.accountable_type == "Band"
+                redirect_to band_path
+            # else, redirect to user_path
+            else 
+                redirect_to user_path
+            end
         end
     end
 
