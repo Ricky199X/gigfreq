066234dddef950b563a0daaeee8f3c724ec7883b
Ricky199X <rickyrojas19@gmail.com>
2019-10-14 14:32:59 -0400

    updated create method for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,25 +5,40 @@ class SessionsController < ApplicationController
 
     end
 
-    def create
-        @account = Account.find_by(email: params[:email])
-        if @account = nil
-            render :new
-        # binding.pry
-        else !@account.nil? && @account.authenticate(params[:account][:password])
-            log_in(@account)
-            # if accountable_type == "Band", redirect to band path
-            if @account.accountable_type == "Band"
-                redirect_to band_path
-            # else, redirect to user_path
-            else 
-                redirect_to user_path
-            end 
-        end
-    end
+    # def create
+    #     @account = Account.find_by(email: params[:email])
+    #     if @account = nil
+    #         render :new
+    #     # binding.pry
+    #     else !@account.nil? && @account.authenticate(params[:account][:password])
+    #         log_in(@account)
+    #         # if accountable_type == "Band", redirect to band path
+    #         if @account.accountable_type == "Band"
+    #             redirect_to band_path
+    #         # else, redirect to user_path
+    #         else 
+    #             redirect_to user_path
+    #         end 
+    #     end
+    # end
 
     def logout
         session.delete(:account_id)
         redirect_to root_path
     end
 end
+
+    def create 
+        if !@account.nil? && @account.authenticate(params[:account][:password])
+            @account = Account.find_by(email: params[:email])
+                log_in(@account)
+            if @account.accountable_type == "Band"
+                redirect_to band_path
+            else 
+                redirect_to user_path
+            end 
+        else 
+        # NOW handle the account being nil or not validated
+            render :new
+        end
+    end
