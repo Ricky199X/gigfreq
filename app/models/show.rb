9e9e7596461cbd1b7667890f3efd9e8aa0cdeb20
Ricky199X <rickyrojas19@gmail.com>
2019-10-16 18:33:33 -0400

    shows associating with bands now

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ApplicationRecord
-    belongs_to :band
+    belongs_to :band, optional: true
     has_many :bands, through: :band_shows
     has_many :user_bands
     has_many :users, through: :user_bands
