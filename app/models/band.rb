63c39ddc271843bee5aab6e59cd4e09d87f2c27f
Ricky199X <rickyrojas19@gmail.com>
2019-10-16 07:23:14 -0400

    can't associate band to show instance

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,9 @@
 class Band < ApplicationRecord
+
+    validates :band_id, presence: true
+    
     has_one :account, as: :accountable
-    has_many :shows
+    has_many :band_shows
     has_many :shows, through: :band_shows
     has_many :user_bands
     has_many :users, through: :user_bands
