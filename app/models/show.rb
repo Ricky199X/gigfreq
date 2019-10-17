78c97e18702fed1cfc02e0ffd8df600f7c26c61a
Ricky199X <rickyrojas19@gmail.com>
2019-10-17 15:51:27 -0400

    updates user <-> show relationship

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ApplicationRecord
     belongs_to :band, optional: true
     has_many :bands, through: :band_shows
-    has_many :user_bands
-    has_many :users, through: :user_bands
+    has_many :user_shows
+    has_many :users, through: :user_shows
 end
