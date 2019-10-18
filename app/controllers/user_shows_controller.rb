d1bc52154353e5fb519a23068dee258bf2b52d5b
Ricky199X <rickyrojas19@gmail.com>
2019-10-18 07:06:48 -0400

    user_show_params added

--- app/controllers/user_shows_controller.rb
+++ app/controllers/user_shows_controller.rb
@@ -1,19 +1,15 @@
 class UserShowsController < ApplicationController
 
-    def index
-    end
+  def new 
+    user_show = UserShow.create(user_id: params[:user_show][:user_id], show_id: params[:user_show][:show_id])
+    binding.pry
+    redirect_to user_path(user_show.user)
+  end
 
-    def create
-        @show = Show.find(params[:show_id])
-        @user = User.find(params[:user_id])
-        @show.users << @user
-        byebug
-    end
+  private
 
+  def user_show_params
+    params.require(:user_show).permit(:user_id, :show_id)
+end
 
-    private
-
-    def user_show_params
-        params.require(:user_show).permit(:user_id, :show_id)
-    end
 end
