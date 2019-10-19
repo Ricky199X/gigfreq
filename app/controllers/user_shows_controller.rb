b1cee2785f106fb02e6e8ed68b239609b27d8c62
Ricky199X <rickyrojas19@gmail.com>
2019-10-19 19:03:42 -0400

    cleans up band show (home) page

--- app/controllers/user_shows_controller.rb
+++ app/controllers/user_shows_controller.rb
@@ -1,20 +1,6 @@
 class UserShowsController < ApplicationController
 
-  # def index
-  #   current_user
-  #   if current_user.accountable_type == "User"
-  #       # find the current_user logged in 
-  #     @user = User.find(params[:user_id])
-  #       # @shows becomes any shows that the user has associated with their instance
-  #     @shows = @user.shows
-  #   else
-  #     flash[:danger] = "You are not a user!"
-  #       redirect_to root_path
-  #   end
-  # end
-
   def new 
-   
   end
 
   def show 
