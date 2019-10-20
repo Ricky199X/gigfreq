669bad9d0dc4fec7d600acd1ac993a5025f35636
Ricky199X <rickyrojas19@gmail.com>
2019-10-19 20:12:22 -0400

    adds new action to usershows controller

--- app/controllers/user_shows_controller.rb
+++ app/controllers/user_shows_controller.rb
@@ -3,13 +3,5 @@ class UserShowsController < ApplicationController
   def new 
   end
 
-  def show 
-  end
-
-  private
-
-  def user_show_params
-    params.require(:user_show).permit(:user_id, :show_id)
-end
 
 end
