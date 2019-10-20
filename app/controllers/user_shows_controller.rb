5bc5b45f3ee825aaa59d9d873ace474f1339d95a
Ricky199X <rickyrojas19@gmail.com>
2019-10-19 20:53:03 -0400

    hidden field tag getting correct current_user.id

--- app/controllers/user_shows_controller.rb
+++ app/controllers/user_shows_controller.rb
@@ -2,7 +2,7 @@ class UserShowsController < ApplicationController
 
   def new 
     user_show = UserShow.create(user_id: params[:user_show][:user_id], show_id: params[:user_show][:show_id])
-    redirect_to user_shows_path(user_show.user)
+    redirect_to user_shows_path(current_user.id)
   end
 
 
