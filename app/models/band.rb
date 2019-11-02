8e923743025a13304345f4a6e1aa735d2d841b55
Ricky199X <rickyrojas19@gmail.com>
2019-11-02 10:26:42 -0400

    band model has_many shows - scope working

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,18 +1,12 @@
 class Band < ApplicationRecord
     
     has_one :account, as: :accountable
-    has_many :band_shows
-    has_many :shows, through: :band_shows
+    has_many :shows
     has_many :user_bands
     has_many :users, through: :user_bands
 
     def name
         self.account.username
-    end
-
-    def shows
-        # needs to find all shows where band_id == self.id
-        Show.select {|gig| gig.band_id == self.id}
     end
 
     def fans
